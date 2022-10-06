0715b631d4773fa4df81e5bdc970842108d0c04b
kimkwatson <kimwatson731@gmail.com>
2022-10-05 23:32:10 -0700

    fixed date functions

--- scripts/task3.js
+++ scripts/task3.js
@@ -66,16 +66,13 @@ divideButton.addEventListener("click", divideNumbers);
 /* BUILT-IN METHODS */
 
 // Step 1: Declare and instantiate a variable of type Date to hold the current date
-let currentDate = Date();
 
 // Step 2: Declare a variable to hold the current year
-let currentYear;
 
 // Step 3: Using the variable declared in Step 1, call the built-in getFullYear() method/function and assign it to the variable declared in Step 2
-currentYear = currentDate.prototype.getFullYear();
 
 // Step 4: Assign the current year variable to an HTML form element with an ID of year
-document.querySelector('#year').innerHTML = currentYear;
+document.querySelector('#year').innerHTML = new Date().getFullYear;
 
 /* ARRAY METHODS */
 
